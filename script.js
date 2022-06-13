81de4815620b748880cc1ee66a930fdd90f2e109
AQIBYASEENBHAT <AQIBYASEENBHAT@GMAIL.COM>
2022-06-13 12:49:11 +0530

    changed from loop to using getAttribut and setAttribute

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
+document.getElementById("divOne").addEventListener("click",function(e){
+document.getElementById("mainImage").src = e.target.getAttribute("src");
+ })
 
 
+/*
+
+using both getAttribute() and setAttribute() 
+
+*/
+
+/*
+
+document.getElementById("divOne").addEventListener("click",function(e){
+
+  document.getElementById("mainImage").setAttribute("src",e.target.getAttribute("src"));
+})
 
 
-
-
-
-
-
-
-let mainImage = document.getElementById("mainImage");
-let images = Array.from(document.getElementsByClassName("images"));
-
-console.log(images);
-images.forEach((img) => {
-  img.addEventListener("click", function () {
-    imgId = this.src;
-  });
-});
-
-document.addEventListener("click", function () {
-  // alert(src);
-  mainImage.src = imgId;
-  // mainImage.alt ="imagehere";
-  //    img1.src = src;
-
-  // alert(mainImage.src);
-});
+*/
