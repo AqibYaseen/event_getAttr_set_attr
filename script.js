c901d2b063047e2a4e1975ead89599aab620d1bb
AQIBYASEENBHAT <AQIBYASEENBHAT@GMAIL.COM>
2022-06-13 10:34:12 +0530

    commit in morning mondat 12-June-22

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 
-    
-    
-    
-    let mainImage = document.getElementById("mainImage");
-    let images = Array.from(document.getElementsByClassName("images"));
 
 
-    images.forEach((i)=>{
-        i.addEventListener("click", function (){
-            src=this.src;
-        })
-    })
 
-   document.addEventListener("click", function (){
-       mainImage.src= src;
-   })
+
+
+
+
+
+
+let mainImage = document.getElementById("mainImage");
+let images = Array.from(document.getElementsByClassName("images"));
+
+console.log(images);
+images.forEach((img) => {
+  img.addEventListener("click", function () {
+    imgId = this.src;
+  });
+});
+
+document.addEventListener("click", function () {
+  // alert(src);
+  mainImage.src = imgId;
+  // mainImage.alt ="imagehere";
+  //    img1.src = src;
+
+  // alert(mainImage.src);
+});
